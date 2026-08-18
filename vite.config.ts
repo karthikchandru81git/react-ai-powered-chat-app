f4d42c6054d6297a308648af9ed2c4c8916c2ac9
karthikchandru81 <karthikchandru81@gmail.com>
2026-08-18 12:02:44 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import tawilwindTypography from '@tailwindcss/typography'
+//import tawilwindTypography from '@tailwindcss/typography'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tawilwindTypography],
+  plugins: [react(), tailwindcss()],
 })
